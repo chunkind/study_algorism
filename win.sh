7db92b4f4cefca22d066128cd05f3f73634381ff
chunkind <chunkind88@gmail.com>
2019-05-29 16:28:53 +0900

    컴파일 추가.

--- win.sh
+++ win.sh
@@ -1,6 +1,7 @@
 echo compile starting...
 javac -cp ./bin -d ./bin src/util/*.java
-javac -cp ./bin -d ./bin src/study/day02/q01/PrimeNumber.java
-javac -cp ./bin -d ./bin src/study/day02/Start.java
+javac -cp ./bin -d ./bin src/study/day02/q01/*.java
+javac -cp ./bin -d ./bin src/study/day02/q02/*.java
+javac -cp ./bin -d ./bin src/study/day02/*.java
 echo service starting...
 java -cp ./bin study.day02.Start
