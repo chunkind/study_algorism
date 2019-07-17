517f80fc5595cf51ef395813e942b3dc07eb4a0e
chunkind <chunkind88@gmail.com>
2019-07-17 13:01:16 +0900

    두번 풀이중...

--- src/sw/day02/sw1979/SW1979_어디에_단어가_들어갈수_있을까2.java
+++ src/sw/day02/sw1979/SW1979_어디에_단어가_들어갈수_있을까2.java
@@ -9,32 +9,41 @@ public class SW1979_어디에_단어가_들어갈수_있을까2 {
 		int[] result = new int[T];
 		for(int a=0; a<T; a++) {
 			int N = sc.nextInt(); //정사각형의 크기
+			int K = sc.nextInt(); //찾는 단어 크기
 			int[][] arr = new int[N][N];
-			int cnt = 0;
-			int searchCnt = 0;
 			for(int b=0; b<N; b++) //입력
 				for(int c=0; c<N; c++)
 					arr[b][c] = sc.nextInt();
-			for(int b=0; b<N; b++) {
-				for(int c=0; c<N; c++) {
-					if(arr[b][c] == 0) cnt = 0;
-					if(arr[b][c] == 1) cnt++;
-				}
-				if(cnt == 3) searchCnt++;
-				cnt = 0;
-			}
-			for(int b=0; b<N; b++) {
-				for(int c=0; c<N; c++) {
-					if(arr[c][b] == 0) cnt = 0;
-					if(arr[c][b] == 1) cnt++;
-				}
-				if(cnt == 3) searchCnt++;
-				cnt = 0;
-			}
-			result[a] = searchCnt;
+			result[a] = getCnt(N, K, arr, true) + getCnt(N, K, arr, false);
 		}
 		for(int a=0; a<T; a++) {
 			System.out.printf("#%d %d\n", a+1, result[a]);
 		}
+	}
+	public static int getCnt(int N, int K, int[][] arr, boolean isSero) {
+		int searchCnt = 0;
+		for(int b=0; b<N; b++) {
+			int cnt = 0;
+			for(int c=0; c<N; c++) {
+				if(isSero) {
+					if(cnt == K && arr[b][c] == 0) {
+						searchCnt++;
+						cnt = 0;
+					}
+					if(arr[b][c] == 1) cnt++;
+				}else {
+					if(cnt == K && arr[c][b] == 0) {
+						searchCnt++;
+						cnt = 0;
+					}
+					if(arr[c][b] == 1) cnt++;
+				}
+				if(cnt == K) {
+					searchCnt++;
+					cnt = 0;
+				}
+			}
+		}
+		return searchCnt;
 	}
 }
