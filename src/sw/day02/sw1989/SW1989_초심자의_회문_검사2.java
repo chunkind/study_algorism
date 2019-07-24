263c3ee3b3c626143e4fae70aff60d78f53a9207
chunkind <chunkind88@gmail.com>
2019-07-24 18:01:45 +0900

    3일차 진행중...

---
+++ src/sw/day02/sw1989/SW1989_초심자의_회문_검사2.java
@@ -0,0 +1,11 @@
+package sw.day02.sw1989;
+
+import java.util.Scanner;
+
+public class SW1989_초심자의_회문_검사2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+	}
+}
