f12a68d7b7c0b86d359511a65c56c35db1127f45
chunkind <chunkind88@gmail.com>
2019-07-30 07:22:05 +0900

    4일차 알고리즘 준비중

---
+++ src/sw/day03/sw1859/백만_장자_프로젝트3.java
@@ -0,0 +1,19 @@
+package sw.day03.sw1859;
+
+import java.util.Scanner;
+
+public class 백만_장자_프로젝트3 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int tc=0; tc<T; tc++) {
+			int N = sc.nextInt();
+			int[] map = new int[N];
+			for(int i=0; i<N; i++) map[i] = sc.nextInt();
+			long max = map[N-1], sum=0;
+			for(int i=N-2; i<=0; i--) {
+				
+			}
+		}
+	}
+}
