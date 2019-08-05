d3dc57487781479124382802e901422666810821
chunkind <chunkind88@gmail.com>
2019-08-05 18:11:16 +0900

    두개의숫자 진행중

---
+++ src/sw/d2/sw1959/SW1959_두개의숫자열.java
@@ -0,0 +1,12 @@
+package sw.d2.sw1959;
+
+import java.util.Scanner;
+
+public class SW1959_두개의숫자열 {
+	public static void main(String[] args) {
+		
+	}
+	public static void process() {
+		Scanner sc = new Scanner(System.in);
+	}
+}
