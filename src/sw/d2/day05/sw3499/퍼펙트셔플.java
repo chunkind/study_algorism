c2e311d965858b0cadcff40e574571af89035cd1
chunkind <chunkind88@gmail.com>
2019-08-12 14:32:06 +0900

    간단한 압축 문제풀이중...

--- src/sw/d2/day05/sw3499/퍼펙트셔플.java
+++ src/sw/d2/day05/sw3499/퍼펙트셔플.java
@@ -10,20 +10,15 @@ public class 퍼펙트셔플 {
 			int n = sc.nextInt();
 			String[] map = new String[n];
 			String[] smap = new String[n];
-			for(int i=0; i<n; i++) {
+			for(int i=0; i<n; i++)
 				map[i] = sc.next();
-			}
 			for(int i=0; i<n; i++) {
-				if(i%2==0) {
-					smap[i] = map[i/2];
-				}else {
-					smap[i] = map[((n-i)/2)+i];
-				}
+				if(i%2==0) smap[i] = map[i/2];
+				else smap[i] = map[((n-i)/2)+i];
 			}
 			System.out.printf("#%d", tc);
-			for(int i=0; i<n; i++) {
+			for(int i=0; i<n; i++)
 				System.out.printf(" %s", smap[i]);
-			}
 			System.out.println();
 		}
 	}
